9507c94f2b18fcaf8c68448d6e2cd4ae46cb0221
Shuga@ <isaacaddyasare@gmail.com>
2022-11-13 20:43:18 +0000

    trying something out

---
+++ mull.java
@@ -0,0 +1,7 @@
+public class mull {
+    public static void main() {
+        System.out.println("Hello");
+        
+    }
+    
+}
---
+++ neww.java
@@ -0,0 +1,9 @@
+public class neww {
+    public static void main(String[]args){
+         int number[] = {12, 34, 55, 90, 66, 45}
+         int sum = 0;
+         double average;
+         while (sum)
+    }
+    
+}
